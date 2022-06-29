4a2d550a43a11e1b3416835cbd5341be6168ec40
vyrwu <dev.anowak@gmail.com>
2022-06-29 15:29:58 +0200

    Apply formatting changes

--- index.ts
+++ index.ts
@@ -9,24 +9,28 @@ const appName = 'portfolio-website'
 
 const tags = {
   App: appName,
-  Environment: environment
+  Environment: environment,
 }
 
 // ISSUE ACM CERTIFICATE
-const eastRegion = new aws.Provider("east", {
+const eastRegion = new aws.Provider('east', {
   profile: aws.config.profile,
   region: 'us-east-1', // Per AWS, ACM certificate must be in the us-east-1 region.
-});
-const cert = new aws.acm.Certificate(domainName, {
-  domainName: domainName,
-  validationMethod: 'DNS',
-  subjectAlternativeNames: [`www.${domainName}`],
-  tags: tags,
-}, { provider: eastRegion })
+})
+const cert = new aws.acm.Certificate(
+  domainName,
+  {
+    domainName: domainName,
+    validationMethod: 'DNS',
+    subjectAlternativeNames: [`www.${domainName}`],
+    tags: tags,
+  },
+  { provider: eastRegion }
+)
 
 const zone = aws.route53.getZone({
   name: domainName,
-  privateZone: false
+  privateZone: false,
 })
 
 const records = cert.domainValidationOptions.apply((dvos) => {
@@ -38,154 +42,169 @@ const records = cert.domainValidationOptions.apply((dvos) => {
       ttl: 600, // 10 minutes
       type: aws.route53.RecordType[dvo.resourceRecordType as keyof typeof aws.route53.RecordType],
       zoneId: zone.then((z: aws.route53.GetZoneResult) => z.id),
-    });
+    })
   })
 })
 
-const certValidation = new aws.acm.CertificateValidation(domainName, {
-  certificateArn: cert.arn,
-  validationRecordFqdns: records.apply((rs: aws.route53.Record[]) => rs.map((r: aws.route53.Record) => r.fqdn)),
-}, { dependsOn: cert, provider: eastRegion });
+const certValidation = new aws.acm.CertificateValidation(
+  domainName,
+  {
+    certificateArn: cert.arn,
+    validationRecordFqdns: records.apply((rs: aws.route53.Record[]) =>
+      rs.map((r: aws.route53.Record) => r.fqdn)
+    ),
+  },
+  { dependsOn: cert, provider: eastRegion }
+)
 
 // CREATE S3 BUCKET
-const contentBucket = new aws.s3.Bucket('content', {
-  bucket: domainName,
-  website: {
-    indexDocument: 'index.html',
+const contentBucket = new aws.s3.Bucket(
+  'content',
+  {
+    bucket: domainName,
+    website: {
+      indexDocument: 'index.html',
+    },
+    tags: tags,
   },
-  tags: tags
-}, { dependsOn: certValidation });
+  { dependsOn: certValidation }
+)
 
 function publicReadPolicyForBucket(bucketName: string): aws.iam.PolicyDocument {
-    return {
-        Version: '2012-10-17',
-        Statement: [{
-            Effect: 'Allow',
-            Principal: '*',
-            Action: [
-                's3:GetObject'
-            ],
-            Resource: [
-                `arn:aws:s3:::${bucketName}/*` // policy refers to bucket name explicitly
-            ]
-        }]
-    };
+  return {
+    Version: '2012-10-17',
+    Statement: [
+      {
+        Effect: 'Allow',
+        Principal: '*',
+        Action: ['s3:GetObject'],
+        Resource: [
+          `arn:aws:s3:::${bucketName}/*`, // policy refers to bucket name explicitly
+        ],
+      },
+    ],
+  }
 }
 
 new aws.s3.BucketPolicy('content-policy', {
   bucket: contentBucket.bucket,
   policy: contentBucket.bucket.apply(publicReadPolicyForBucket),
-});
+})
 
-let siteFilesDir = 'www'; // directory for content files
+const siteFilesDir = 'www' // directory for content files
 
 getAllFiles(siteFilesDir).map((filePath: string) => {
   return new aws.s3.BucketObject(filePath.replace('www/', ''), {
     bucket: contentBucket,
     source: new pulumi.asset.FileAsset(filePath),
     contentType: mime.getType(filePath) || undefined,
-    tags: tags
+    tags: tags,
   })
 })
 
 // CREATE CLOUDFRONT DISTIBUTON
 const logsBucket = new aws.s3.Bucket('requestLogs', {
   bucket: `${domainName}-logs`,
-  acl: "private",
+  acl: 'private',
   tags: tags,
-});
+})
 
 const originAccessIdentity = new aws.cloudfront.OriginAccessIdentity('s3-private-access', {
-  comment: "this is needed to setup s3 polices and make s3 not public.",
-});
+  comment: 'this is needed to setup s3 polices and make s3 not public.',
+})
 
 const cdn = new aws.cloudfront.Distribution('cdn', {
-    enabled: true,
-    aliases: [`www.${domainName}`, domainName],
-    origins: [
-        {
-            originId: contentBucket.arn,
-            domainName: contentBucket.websiteEndpoint,
-        //     s3OriginConfig: {
-        //         originAccessIdentity: originAccessIdentity.cloudfrontAccessIdentityPath,
-        // },
-        customOriginConfig: {
-            httpPort: 80,
-            httpsPort: 443,
-            originProtocolPolicy: 'http-only',
-            originSslProtocols: ["TLSv1", "TLSv1.1", "TLSv1.2"]
-          }
-        },
+  enabled: true,
+  aliases: [`www.${domainName}`, domainName],
+  origins: [
+    {
+      originId: contentBucket.arn,
+      domainName: contentBucket.websiteEndpoint,
+      //     s3OriginConfig: {
+      //         originAccessIdentity: originAccessIdentity.cloudfrontAccessIdentityPath,
+      // },
+      customOriginConfig: {
+        httpPort: 80,
+        httpsPort: 443,
+        originProtocolPolicy: 'http-only',
+        originSslProtocols: ['TLSv1', 'TLSv1.1', 'TLSv1.2'],
+      },
+    },
   ],
 
-    defaultRootObject: "index.html",
+  defaultRootObject: 'index.html',
 
-    // A CloudFront distribution can configure different cache behaviors based on the request path.
-    // Here we just specify a single, default cache behavior which is just read-only requests to S3.
-    defaultCacheBehavior: {
-        targetOriginId: contentBucket.arn,
-        viewerProtocolPolicy: "redirect-to-https",
-        allowedMethods: ["GET", "HEAD", "OPTIONS"],
-        cachedMethods: ["GET", "HEAD", "OPTIONS"],
-        forwardedValues: {
-            cookies: { forward: "none" },
-            queryString: false,
-        },
-        minTtl: 0,
-        defaultTtl: 600, // 10 minutes
-        maxTtl: 600, // 10 minutes
+  // A CloudFront distribution can configure different cache behaviors based on the request path.
+  // Here we just specify a single, default cache behavior which is just read-only requests to S3.
+  defaultCacheBehavior: {
+    targetOriginId: contentBucket.arn,
+    viewerProtocolPolicy: 'redirect-to-https',
+    allowedMethods: ['GET', 'HEAD', 'OPTIONS'],
+    cachedMethods: ['GET', 'HEAD', 'OPTIONS'],
+    forwardedValues: {
+      cookies: { forward: 'none' },
+      queryString: false,
     },
-    priceClass: "PriceClass_100",
-    restrictions: {
-        geoRestriction: {
-            restrictionType: "none",
-        },
+    minTtl: 0,
+    defaultTtl: 600, // 10 minutes
+    maxTtl: 600, // 10 minutes
+  },
+  priceClass: 'PriceClass_100',
+  restrictions: {
+    geoRestriction: {
+      restrictionType: 'none',
     },
-    viewerCertificate: {
-        acmCertificateArn: cert.arn,
-        sslSupportMethod: "sni-only",
-    },
-    loggingConfig: {
-        bucket: logsBucket.bucketDomainName,
-        includeCookies: false,
-        prefix: `${domainName}/`,
-    },
-    tags: tags,
-});
+  },
+  viewerCertificate: {
+    acmCertificateArn: cert.arn,
+    sslSupportMethod: 'sni-only',
+  },
+  loggingConfig: {
+    bucket: logsBucket.bucketDomainName,
+    includeCookies: false,
+    prefix: `${domainName}/`,
+  },
+  tags: tags,
+})
 
 new aws.route53.Record(
   `cdn-${domainName}`,
   {
-      name: domainName,
-      zoneId: zone.then((z: aws.route53.GetZoneResult) => z.id),
-      type: 'A',
-      aliases: [
-          {
-              name: cdn.domainName,
-              zoneId: cdn.hostedZoneId,
-              evaluateTargetHealth: true,
-          },
-      ],
+    name: domainName,
+    zoneId: zone.then((z: aws.route53.GetZoneResult) => z.id),
+    type: 'A',
+    aliases: [
+      {
+        name: cdn.domainName,
+        zoneId: cdn.hostedZoneId,
+        evaluateTargetHealth: true,
+      },
+    ],
   },
   { dependsOn: cert }
 )
 
-
 new aws.route53.Record(
   `cdn-www.${domainName}`,
   {
-      name: `www.${domainName}`,
-      zoneId: zone.then((z: aws.route53.GetZoneResult) => z.id),
-      type: 'A',
-      aliases: [
-          {
-              name: cdn.domainName,
-              zoneId: cdn.hostedZoneId,
-              evaluateTargetHealth: true,
-          },
-      ],
+    name: `www.${domainName}`,
+    zoneId: zone.then((z: aws.route53.GetZoneResult) => z.id),
+    type: 'A',
+    aliases: [
+      {
+        name: cdn.domainName,
+        zoneId: cdn.hostedZoneId,
+        evaluateTargetHealth: true,
+      },
+    ],
   },
   { dependsOn: cert }
 )
 
 export const websiteUrls = cdn.aliases
+
+// TODO:
+// - Make S3 private
+// - Retain 7 days of access logs
+// - Price alert
+// - Consider writing about alerting project on Medium, or as portfolio element.
--- utils.ts
+++ utils.ts
@@ -4,11 +4,11 @@ import * as path from 'path'
 export const getAllFiles = (dirPath: string, arrayOfFiles: string[] = []) => {
   const files = fs.readdirSync(dirPath)
 
-  files.forEach(function(file) {
-    if (fs.statSync(dirPath + "/" + file).isDirectory()) {
-      arrayOfFiles = getAllFiles(dirPath + "/" + file, arrayOfFiles)
+  files.forEach(function (file) {
+    if (fs.statSync(dirPath + '/' + file).isDirectory()) {
+      arrayOfFiles = getAllFiles(dirPath + '/' + file, arrayOfFiles)
     } else {
-      arrayOfFiles.push(path.join(dirPath, "/", file))
+      arrayOfFiles.push(path.join(dirPath, '/', file))
     }
   })
 
